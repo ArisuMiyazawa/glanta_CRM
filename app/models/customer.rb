d25724502f229444222c67240927a00afd09fee4
Arisu Miyazawa <payphone.omn.05.06@gmail.com>
2021-02-03 15:30:06 +0900

    メールアドレスに正規表現、名前に文字数のvalidation追加

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,11 +1,15 @@
 class Customer < ApplicationRecord
-  validates :name, presence: true
-  validates :name_kana, presence: true
+  validates :name, presence: true, length: {maximum: 15}
+  validates :name_kana, presence: true, length: {maximum: 15}
   validates :birthday, presence: true
   validates :job, presence: true
   validates :postcode, presence: true
   validates :street_address, presence: true
   validates :phone_number, presence: true
-  validates :email, presence: true
+
+  VALID_EMAIL_REGEX = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/
+  validates :email, presence: true, format: {with: VALID_EMAIL_REGEX}
+
   validates_acceptance_of :agreement, allow_nil: false, on: :create
+
 end
