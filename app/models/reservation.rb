837beb3d6ea38bd0f418bef7fc28d5a898212b4c
Arisu Miyazawa <payphone.omn.05.06@gmail.com>
2021-03-18 15:39:49 +0900

    予約一覧ページ編集・空カルテページ作成

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -3,19 +3,4 @@ class Reservation < ApplicationRecord
   validates :reservation_time, presence: true
   belongs_to :customer
   has_one :treatment_record
-
-  def self.search(search)
-    search ? where('name_kana LIKE ?', "%#{search}%") : all
-  end
-
-  validate :date_before_start
-  validates :start_time, uniqueness: { message: 'は他のユーザーが予約しています' }
-
-  def start_time
-    self.reservation_time
-  end
-
-  def date_before_start
-    errors.add(:start_time, "は過去の日付を選択できません") if start_time < Date.today
-  end
 end
